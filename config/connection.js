e2ad42000df2812123e36a2180a459bab26d8427
Adrit <axb5095@gmail.com>
2018-11-15 19:48:36 -0500

    committing changes

--- config/connection.js
+++ config/connection.js
@@ -4,8 +4,8 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "sulnwdk5uwjw1r2k.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
-  user: "q0fa5z0e6qnzwwd6	",
-  password: "fpecghhyfcl1eytm",
+  user: "q0fa5z0e6qnzwwd6",
+  password: "ybny8oq1xp0p3x53",
   database: "iuid9v3nz7712wr0"
 });
 
